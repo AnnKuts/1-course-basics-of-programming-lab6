9c52b4247a2be5168602c254c79ae0cb284f09d3
Ann Kuts <annkuts@Anns-MacBook-Pro.local>
2024-12-09 19:48:40 +0200

    made all tasks

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,23 @@
-'use strict';
+"use strict";
 
-const pipe = (...fns) => (x) => null;
+const pipe = (...fns) => {
+  for (const fn of fns) {
+    if(typeof(fn) !== "function") throw new Error("Not a function");
+  }
+ return (x) => fns.reduce((value, fn) => fn(value), x);
+};
+
+// const inc = x => ++x;
+// const twice = x => x * 2;
+// const cube = x => x ** 3;
+
+// const f = pipe(inc, twice, cube);
+// const x = f(5); 
+// console.log(x); //1728
+
+// const f = pipe(inc, 7, cube);
+// const x = f(5);
+// console.log(x); //Not a function
+// //decided to check on my own, tests still are correct
 
 module.exports = { pipe };
--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,28 @@
-'use strict';
+"use strict";
 
-const compose = (...fns) => (x) => null;
+const compose = (...fns) => {
+  const handlers = [];
+  const composed = (x) => {
+    if (fns.length === 0) return x;
+    let result = x;
+    try {
+      for (let i = fns.length - 1; i >= 0; i--) {
+        result = fns[i](result);
+      }
+      return result;
+    } catch (error) {
+      for (const handler of handlers) {
+        handler(error);
+      }
+      return undefined;
+    }
+  };
+  composed.on = (eventName, handler) => {
+    if (eventName === "error" && typeof(handler) === "function") {
+      handlers.push(handler);
+    }
+  };
+  return composed;
+};
 
 module.exports = { compose };
